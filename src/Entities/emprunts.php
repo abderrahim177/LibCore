2d3767cc90c67d52c8e1023de121216c568210e6
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-05-06 17:32:07 +0100

    add new branch

---
+++ src/Entities/emprunts.php
@@ -0,0 +1,27 @@
+<?php
+class emprunts {
+    private $dateemprunt;
+    private $dateretoure;
+    private $id_book;
+    private $id_member;
+
+    public function __construct($dateemprunt,$dateretoure, $id_book,$id_member)
+    {
+        $this->dateemprunt = $dateemprunt;
+        $this->dateretoure = $dateretoure;
+        $this->id_book = $id_book;
+        $this->id_member = $id_member;
+    }
+    public function getDateemprunt(){
+        return $this->dateemprunt;
+    }
+    public function getDateretoure(){
+        return $this->dateretoure;
+    }
+     public function getIdBook(){
+        return $this->id_book;
+    }
+     public function getIdMemeber(){
+        return $this->id_member;
+    }
+}
