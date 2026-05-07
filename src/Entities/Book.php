62a8b047a15cf0d4f390aa281b799ad2916564eb
mohamed_bnz <simobnz81@gmail.com>
2026-05-07 12:44:11 +0100

    Add getters and setters for Book class properties

--- src/Entities/Book.php
+++ src/Entities/Book.php
@@ -23,4 +23,57 @@ class Book {
 
 
 
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getIsbn() {
+        return $this->isbn;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getIsAvailable() {
+        return $this->isAvailable;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function getIdLibrary() {
+        return $this->idLibrary;
+    }
+
+ 
+
+    public function setIsbn($isbn) {
+        $this->isbn = $isbn;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    public function setAvailable($status) {
+        $this->isAvailable = $status;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
+
+    public function setIdLibrary($idLibrary) {
+        $this->idLibrary = $idLibrary;
+    }
 }
