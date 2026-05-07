aba4d5a67acfb57f610065d02af9542c9e2bbac8
mohamed_bnz <simobnz81@gmail.com>
2026-05-07 12:43:20 +0100

    Create Book class with basic structure and properties

---
+++ src/Entities/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+class Book {
+
+    private $id;
+    private $isbn;
+    private $title;
+    private $author;
+    private $isAvailable;
+    private $status;
+    private $idLibrary;
+
+   
+}
