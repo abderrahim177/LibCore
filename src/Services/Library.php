90e33b73f0abf80a108d59f7cc907de655151e9a
mohamed_bnz <simobnz81@gmail.com>
2026-05-07 12:51:03 +0100

    Add feature to insert new book into database

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -12,4 +12,24 @@ class Library
         $this->pdo = $pdo;
     }
 
+    public function addBook()
+    {
+        $isbn = readline("ISBN: ");
+        $title = readline("Titre: ");
+        $author = readline("Auteur: ");
+        $idLibrary = readline("ID Library: ");
+        $sql = "INSERT INTO books (isbn, title, author, id_library)
+            VALUES (:isbn, :title, :author, :id_library)";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            ':isbn' => $isbn,
+            ':title' => $title,
+            ':author' => $author,
+            ':id_library' => $idLibrary
+        ]);
+
+        echo "Livre ajouté";
+    }
 }
