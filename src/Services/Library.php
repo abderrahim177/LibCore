97031d1ea5e739eaf0ebdb64c786c35c98b8b62b
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-05-07 10:59:15 +0100

    add function of search book

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -2,4 +2,26 @@
 
 require_once '../config/database.php';
 
+class Library {
 
+    private $db;
+
+    public function __construct($dbConnection) {
+        $this->db = $dbConnection;
+    }
+
+    public function searchBook($keyword){
+
+        $sql = "SELECT * FROM books 
+                WHERE title LIKE :kw
+                OR isbn LIKE :kw";
+
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->execute([
+            'kw' => "%$keyword%"
+        ]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
