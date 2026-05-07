4bf08e46f602abd7c8a88835085a8096aae7c384
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-05-07 15:38:58 +0100

    add function of getBorrowedBooks

--- mainMember.php
+++ mainMember.php
@@ -14,6 +14,9 @@ while (true) {
 
     echo "1. Search Book\n";
     echo "2. Exit\n";
+    echo "3. returner\n";
+    echo "4. My Borrowed Books\n";
+    echo "5. Exit\n";
     $choice = readline("Choose option: ");
     if ($choice == 1) {
         $q = readline("Enter title or ISBN: ");
@@ -41,8 +44,48 @@ while (true) {
     }
 }
 
-    else {
+     elseif ($choice == 3) {
 
-        echo "Invalid option\n";
+        $isbn = readline("Enter ISBN: ");
+
+        $result = $library->returnBook($isbn);
+
+        if ($result) {
+
+            echo "✔ Book returned successfully\n";
+        }
+        else {
+
+            echo " Return failed\n";
+        }
     }
+
+    // EXIT
+    elseif ($choice == 4) {
+
+    $memberId = readline("Enter Member ID: ");
+
+    $books = $library->getBorrowedBooks($memberId);
+
+    if ($books) {
+
+        echo "\n=== MY BOOKS ===\n";
+
+        foreach ($books as $book) {
+
+            echo "Title: " . $book['title'] . "\n";
+            echo "ISBN: " . $book['isbn'] . "\n";
+            echo "Borrow Date: " . $book['borrow_date'] . "\n";
+            echo "----------------------\n";
+        }
+    }
+        else {
+
+            echo "❌ No borrowed books\n";
+        }
+    }
+    else{
+        echo "not fount";
+    }
+
 }
--- src/Services/Library.php
+++ src/Services/Library.php
@@ -7,8 +7,7 @@ class Library {
     public function __construct($dbConnection) {
         $this->db = $dbConnection;
     }
-
-    // 🔎 Search Book (one result only)
+    //  Search Book 
     public function searchBook($keyword){
 
         $sql = "SELECT * FROM books 
@@ -25,7 +24,7 @@ class Library {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    // 📚 US6: Borrow Book
+    //  US6: Borrow Book
 public function borrowBook($memberId, $isbn){
 
     $isbn = trim($isbn);
@@ -62,5 +61,70 @@ public function borrowBook($memberId, $isbn){
     ]);
 
     return true;
+}
+
+public function returnBook($isbn){
+
+    $isbn = trim($isbn);
+
+    // 1. get book
+    $sql = "SELECT * FROM books WHERE isbn = :isbn";
+
+    $stmt = $this->db->prepare($sql);
+
+    $stmt->execute([
+        'isbn' => $isbn
+    ]);
+
+    $book = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // ❌ book not found
+    if (!$book) {
+        return false;
+    }
+
+    // 2. update availability
+    $update = "UPDATE books
+               SET is_available = 1,
+                   status = 'disponible'
+               WHERE isbn = :isbn";
+
+    $stmt2 = $this->db->prepare($update);
+
+    $stmt2->execute([
+        'isbn' => $isbn
+    ]);
+
+    // 3. delete borrow record
+    $delete = "DELETE FROM emprunts
+               WHERE id_book = :id_book";
+
+    $stmt3 = $this->db->prepare($delete);
+
+    $stmt3->execute([
+        'id_book' => $book['id']
+    ]);
+
+    return true;
+}
+
+    
+public function getBorrowedBooks($memberId){
+
+    $sql = "SELECT books.title,
+                   books.isbn,
+                   emprunts.borrow_date
+            FROM emprunts
+            INNER JOIN books
+            ON emprunts.id_book = books.id
+            WHERE emprunts.id_member = :id_member";
+
+    $stmt = $this->db->prepare($sql);
+
+    $stmt->execute([
+        'id_member' => $memberId
+    ]);
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 }
