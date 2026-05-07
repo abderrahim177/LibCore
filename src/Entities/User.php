1239a314b616293389bb7547389d03e8e9ca8d47
mohamed_bnz <simobnz81@gmail.com>
2026-05-07 12:46:26 +0100

    Add constructor and getters for User class

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -6,4 +6,19 @@ abstract class User {
     private $name;
     private $email;
 
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
 }
