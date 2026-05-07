80a39c3d43cd6f64500ee0b5d171f4a0e64f56e9
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-05-07 14:52:26 +0100

    add function of borrowBook

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -26,38 +26,39 @@ class Library {
     }
 
     // 📚 US6: Borrow Book
- public function borrowBook($memberId, $isbn){
+public function borrowBook($memberId, $isbn){
 
     $isbn = trim($isbn);
 
+    // 1. get book
     $sql = "SELECT * FROM books WHERE isbn = :isbn";
-
     $stmt = $this->db->prepare($sql);
-
     $stmt->execute(['isbn' => $isbn]);
 
     $book = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if (trim(strtolower($book['status'])) !== 'disponible') {
+    if (!$book || $book['is_available'] != 1) {
         return false;
     }
 
+    // 2. update book
     $update = "UPDATE books 
-               SET status = 'emprunté'
+               SET is_available = 0,
+                   status = 'emprunté'
                WHERE isbn = :isbn";
 
     $stmt2 = $this->db->prepare($update);
-
     $stmt2->execute(['isbn' => $isbn]);
 
+    // 3. insert borrow
     $insert = "INSERT INTO emprunts (id_member, id_book, borrow_date)
-               VALUES (:member_id, :isbn, NOW())";
+               VALUES (:id_member, :id_book, NOW())";
 
     $stmt3 = $this->db->prepare($insert);
 
     $stmt3->execute([
         'id_member' => $memberId,
-        'isbn' => $isbn
+        'id_book' => $book['id']
     ]);
 
     return true;
